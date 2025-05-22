1e1ce2aa551db693a8be477c67757d1b7c91c533
Adithyaa Saravanam <adithyaasaravanam@gmail.com>
2025-05-22 15:02:13 +0530

    [Adithyaa] : (feat) : Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+import LandingPage from "./LandingPage";
+import "./App.css"
+export default function App() {
+  return <LandingPage />
+}
---
+++ src/LandingPage.jsx
@@ -0,0 +1,10 @@
+export default function LandingPage(){
+    const align = {
+        textAlign : 'center'
+    }
+    return (
+        <div>
+            <h1 style={align}> Welcome to Blog.exe</h1>
+        </div>
+    )
+}
